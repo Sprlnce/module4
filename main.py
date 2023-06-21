c4208ce71a60f772ea92e0b87914b6342b55eca7
Vladislav <mr.ender555@gmail.com>
2023-06-21 15:11:08 +0500

    init repo

---
+++ main.py
@@ -0,0 +1,9 @@
+def strcounter(s: str):
+    # O(N)
+    syms_counter = {} # словарь
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    
+    print(syms_counter)
+
+strcounter("Злой")
